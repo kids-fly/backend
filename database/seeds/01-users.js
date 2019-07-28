0a3150a31379dbd15709d5ca5b459084ac85f49b
basilcea <ogbonna.basil3@gmmail.com>
2019-07-28 13:16:07 +0100

    seed database

---
+++ database/seeds/01-users.js
@@ -0,0 +1,17 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {id: 1, username : 'cea', password:'b12345',firstname:'basil',lastname:'ogbonna',contact:'1234517',isAdmin:false},
+        {id: 2, username : 'nicleen', password:'n12345',firstname:'nicholas',lastname:'patterson',contact:'55213',isAdmin:false}, 
+        {id: 3, username : 'deAli', password:'d12345',firstname:'duraan',lastname:'ali',contact:'764779776',isAdmin:true},
+        {id: 4, username : 'kesp', password:'k12345',firstname:'keshwan',lastname:'proper',contact:'789990',isAdmin:false},
+        {id: 5, username : 'dbjohn', password:'bj12345',firstname:'dbgoin',lastname:'johnson',contact:'557890',isAdmin:true},
+        {id: 6, username : 'lettmen', password:'l12345',firstname:'letelly',lastname:'menesh',contact:'789980',isAdmin:true},
+        {id: 7, username : 'kim-ryan', password:'k12345',firstname:'kimberly',lastname:'ryan',contact:'55567789',isAdmin:false}, 
+      ]);
+    });
+};
---
+++ database/seeds/05-flights.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('flights').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('flights').insert([
+        {id: 1, departure_airport_id:1,departure_time:'08-09-2019 15:03', arrival_airport_id:2, arrival_time:'08-09-2019 18:03',airline_name:'Emirates'},
+        {id: 2, departure_airport_id:1,departure_time:'09_09_2019 12:00', arrival_airport_id:2, arrival_time:'09_09_2019 16:00',airline_name:'Airpeace'},
+        {id: 3, departure_airport_id:2,departure_time:'09_09_2019 13:00', arrival_airport_id:1, arrival_time:'10_09_2019 09:00',airline_name:"Emirates"},
+      ]);
+    });
+};
