e1d0584feed34d8b191fcf6386fcb9d5cb5b31c4
basilcea <ogbonna.basil3@gmmail.com>
2019-08-01 12:59:36 +0100

    fix error

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -6,10 +6,12 @@ const Users = require('../users/userModel')
 dotenv.config()
 const authenticate =async(req, res, next)=> {
   try{
+    console.log(req.session.user)
     if (req.session && req.session.user) {
       const decrypt = await jwt.verify(req.session.user, process.env.JWT_SECRET)
-     
+     console.log(decrypt)
       const rows = await Users.getUsers(decrypt.subject)
+      console.log(rows)
       if(!rows){
         return statusHandler(res ,403,'Token not accessible')
       }
