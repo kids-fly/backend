129b7ff72bc725236c3595534bf445912ae49fd6
basilcea <ogbonna.basil3@gmmail.com>
2019-08-02 10:19:45 +0100

    seed

--- database/seeds/00-cleanup.js
+++ database/seeds/00-cleanup.js
@@ -1,8 +1,9 @@
-const cleaner = require("knex-cleaner");
-// Deletes ALL existing entries
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return cleaner.clean(knex, {
-    ignoreTables: ["knex_migrations", "knex_migrations_lock"]
-  });
-};
+
+// const cleaner = require('knex-cleaner')
+// exports.seed = function(knex) {
+//   // Deletes ALL existing entries
+//   return cleaner.clean(knex ,{
+//     ignoreTables:['knex_migrations','knex_migrations_lock']
+//   })
+
+// };
--- knexfile.js
+++ knexfile.js
@@ -29,7 +29,7 @@ module.exports = {
       tableName: "dbmigrations"
     },
     seeds: {
-      directory: "../backend/database/seeds"
+      directory: "../database/seeds",
     }
   }
 
